abcff816332b725df3cd0be533dccaef1792d59e
Brendan Doherty <github@sidewinder.co.uk>
2017-12-04 18:28:36 +0000

    day 3

--- 5.test.js
+++ 5.test.js
@@ -1,24 +1,5 @@
-const fs = require("fs");
-const { promisify } = require("util");
-const f = require("./4");
-const readFileAsync = promisify(fs.readFile);
+const f = require("./5");
 
-async function myReadfile() {
-    content = await readFileAsync("./4.data.txt", "utf8");
-}
-
-let content;
-
-describe("Day 5", () => {
-    beforeAll(async () => {
-        await myReadfile();
-    });
-
-    it("part 1", async () => {
-        expect(f(content)).toEqual(455);
-    });
-
-    it("part 2", async () => {
-        expect(f(content, 2)).toEqual(186);
-    });
+it.skip("part 1", async () => {
+    expect(f()).toEqual(true);
 });
