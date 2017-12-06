a1d4869e4f8f4335dd0392c30e4be46dca696852
Brendan Doherty <github@sidewinder.co.uk>
2017-12-06 17:14:26 +0000

    refactor

--- 6.js
+++ 6.js
@@ -37,7 +37,7 @@ const redistribute = step => {
 
             // thunked??
             // http://www.thinkingincrowd.me/2016/06/06/How-to-avoid-Stack-overflow-error-on-recursive/
-            var op = fn; // thunked??
+            var op = fn;
             while (op != null && typeof op === "function") {
                 op = op(spread(step));
             }
