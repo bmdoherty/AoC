aca8c4f58c0b46a7c07acb642076aaa52ef61f93
BM Doherty <bmdoherty@sidewinder.co.uk>
2017-12-16 11:49:23 +0000

    Day 14 test

--- 14.test.js
+++ 14.test.js
@@ -1,23 +1,23 @@
 const f = require("./14");
 
 describe("Day 14", () => {
-    // it("part 1 example", () => {
-    //     let input = Array.from(Array(256).keys());
-    //     let key = "flqrgnkx";
-    //     expect(f(input, key)).toEqual(8108);
-    // });
+    it("part 1 example", () => {
+        let input = Array.from(Array(256).keys());
+        let key = "flqrgnkx";
+        expect(f(input, key)).toEqual(8108);
+    });
 
-    // it("part 1", () => {
-    //     let input = Array.from(Array(256).keys());
-    //     let key = "stpzcrnm";
-    //     expect(f(input, key)).toEqual(8250);
-    // });
+    it("part 1", () => {
+        let input = Array.from(Array(256).keys());
+        let key = "stpzcrnm";
+        expect(f(input, key)).toEqual(8250);
+    });
 
-    // it("part 2 example", () => {
-    //     let input = Array.from(Array(256).keys());
-    //     let key = "flqrgnkx";
-    //     expect(f(input, key, 2)).toEqual(1242);
-    // });
+    it("part 2 example", () => {
+        let input = Array.from(Array(256).keys());
+        let key = "flqrgnkx";
+        expect(f(input, key, 2)).toEqual(1242);
+    });
 
     it("part 2", () => {
         let input = Array.from(Array(256).keys());
